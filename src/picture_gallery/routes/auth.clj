2e3073930e6a9295b53a608f3be8907a60f57142
Gregório Melo <gregorio@gregoriomelo.com>
2014-10-27 21:57:54 +1000

    Handling registration errors

--- src/picture_gallery/routes/auth.clj
+++ src/picture_gallery/routes/auth.clj
@@ -4,25 +4,43 @@
             [picture-gallery.routes.home :refer :all]
             [picture-gallery.views.layout :as layout]
             [noir.session :as session]
-            [noir.response :as resp]))
+            [noir.response :as resp]
+            [noir.validation :as validation]))
+
+(defn valid? [id pass pass1]
+  (validation/rule (validation/has-value? id)
+                   [:id "user id is required"])
+  (validation/rule (validation/min-length? pass 5)
+                   [:pass "password must be at least 5 characters"])
+  (validation/rule (= pass pass1)
+                   [:pass "entered passwords do not match"])
+  (not (validation/errors? :id :pass :pass1)))
+
+(defn error-item [[error]]
+  [:div.error error])
 
 (defn registration-page [& [id]]
   (layout/common
     (form-to [:post "/register"]
+             (validation/on-error :id error-item)
              (label "user-id" "user id")
-             (text-field "id" id)
+             (text-field {:tab-index 1} "id" id)
              [:br]
+             (validation/on-error :pass error-item)
              (label "pass" "password")
-             (password-field "pass")
+             (password-field {:tab-index 2} "pass")
              [:br]
+             (validation/on-error :pass1 error-item)
              (label "pass1" "retype password")
-             (password-field "pass1")
+             (password-field {:tab-index 3} "pass1")
              [:br]
-             (submit-button "create account"))))
+             (submit-button {:tab-index 4} "create account"))))
 
 (defn handle-registration [id pass pass1]
-  (session/put! :user id)
-  (resp/redirect "/"))
+  (if (valid? id pass pass1)
+    (do (session/put! :user id)
+        (resp/redirect "/"))
+    (registration-page id)))
 
 (defroutes auth-routes
   (GET "/register" []
