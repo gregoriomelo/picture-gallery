56832169b710b265d8e2019dc44ceec4883d22a5
Gregório Melo <gregorio@gregoriomelo.com>
2015-01-20 20:54:06 +1000

    Improving the menu

--- src/picture_gallery/views/layout.clj
+++ src/picture_gallery/views/layout.clj
@@ -11,21 +11,26 @@
      (include-css "/css/screen.css")]
     [:body content]))
 
-(defn user-menu [user]
-  (list
-    [:div (link-to "/upload" "upload images")]
-    [:div (link-to "/logout" (str "logout " user))]))
+(defn make-menu [& items]
+  [:div (for [item items] [:div.menuitem item])])
 
 (defn guest-menu []
-  [:div (link-to "/register" "register")
-   (form-to [:post "/login"]
-            (text-field {:placeholder "screen name"} "id")
-            (password-field {:password "password"} "pass")
-            (submit-button "login"))])
+  (make-menu
+    (link-to "/" "home")
+    (link-to "/register" "register")
+    (form-to [:post "/login"]
+             (text-field {:placeholder "screen name"} "id")
+             (password-field {:password "password"} "pass")
+             (submit-button "login"))))
+
+(defn user-menu [user]
+  (make-menu
+    (link-to "/upload" "upload images")
+    (link-to "/logout" (str "logout " user))))
 
 (defn common [& content]
   (base
     (if-let [user (session/get :user)]
       (user-menu user)
       (guest-menu))
-    content))
+    [:div.content content]))
